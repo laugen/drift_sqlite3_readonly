a93a7f13e7d952a51dd5f8028171ed2657c14a1b
Arend Kühle <arend.kuehle@coodoo.de>
2024-01-12 10:17:18 +0100

    using an existing database connection with OpenMode.readOnly from the sqlite3 package, passing it to NativeDatabase.opened to use it with drift

--- lib/database/connection.dart
+++ lib/database/connection.dart
@@ -1,17 +1,31 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:drift/backends.dart';
 import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
 import 'package:drift_dev/api/migrations.dart';
 import 'package:flutter/foundation.dart';
 import 'package:path/path.dart';
+import 'package:sqlite3/sqlite3.dart';
 
-DatabaseConnection connect({required String databaseName}) {
-  return DatabaseConnection.delayed(Future(() async {
-    final File file = databaseFile(databaseName: databaseName);
-    return NativeDatabase.createBackgroundConnection(file, logStatements: true);
-  }));
+// DatabaseConnection connect({required String databaseName}) {
+//   return DatabaseConnection.delayed(Future(() async {
+//     final File file = databaseFile(databaseName: databaseName);
+//     return NativeDatabase.createBackgroundConnection(file, logStatements: true);
+//   }));
+// }
+
+DelegatedDatabase connect({required String databaseName}) {
+  if (true) {
+    String path = databasePath(databaseName: databaseName);
+    Database database = sqlite3.open(path, mode: OpenMode.readOnly, uri: true);
+
+    ResultSet resultSet = database.select('SELECT * FROM tbl1');
+    debugPrint('resultSet: $resultSet');
+
+    return NativeDatabase.opened(database, logStatements: true);
+  }
 }
 
 Future<void> validateDatabaseSchema(GeneratedDatabase database) async {
@@ -20,7 +34,7 @@ Future<void> validateDatabaseSchema(GeneratedDatabase database) async {
   }
 }
 
-File databaseFile({required String databaseName}) {
+String databasePath({required String databaseName}) {
   String path = join(
     Platform.environment['PWD'].toString(),
     'lib',
@@ -28,5 +42,9 @@ File databaseFile({required String databaseName}) {
     databaseName,
   );
   debugPrint('database path: $path');
-  return File(path);
+  return path;
+}
+
+File databaseFile({required String databaseName}) {
+  return File(databasePath(databaseName: databaseName));
 }
